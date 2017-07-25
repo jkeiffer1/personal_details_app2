00cbecc959778b034007b2b0f4423baf0eeeb8ea
jkeiffer1 <saros82@hotmail.com>
2017-07-25 17:25:01 -0400

    added hair color, age, and working on hair color

--- app2.rb
+++ app2.rb
@@ -22,5 +22,21 @@ post '/age' do
 	firstname = params[:firstname]
 	lastname = params[:lastname]
 	age = params[:age]
-	"your name is #{firstname} #{lastname} and you are #{age} years old"
+#	"your name is #{firstname} #{lastname} and you are #{age} years old"
+redirect '/hair?firstname=' + firstname + "&lastname=" + lastname + "&age=" + age
+end
+
+get '/hair' do
+	firstname = params[:firstname]
+	lastname = params[:lastname]
+	age = params[:age]
+	erb :hair, locals:{firstname: firstname, lastname: lastname, age: age}
+end
+
+post '/hair' do
+	firstname = params[:firstname]
+	lastname = params[:lastname]
+	age = params[:age]
+	hair = params[:hair]
+	"your name is #{firstname} #{lastname}, you are #{age} years old, and you have #{hair} hair."
 end
