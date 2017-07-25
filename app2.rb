fcad4fb1384a2a66bc57cceffbd288257c3cbc50
jkeiffer1 <saros82@hotmail.com>
2017-07-25 16:41:44 -0400

    app2 with first/last names, and age

--- app2.rb
+++ app2.rb
@@ -1,7 +1,26 @@
 require "sinatra"
-enable :sessions
 
 get '/' do
  "hello world"
 erb :names
+end
+
+post '/name' do
+	firstname = params[:firstname]
+	lastname = params[:lastname]
+#	"#your firstname is #{firstname} and your last name is #{lastname}"
+redirect '/age?firstname=' + firstname + "&lastname=" + lastname
+end
+
+get '/age' do
+	firstname = params[:firstname]
+	lastname = params[:lastname]
+	erb :age, locals:{firstname: firstname, lastname: lastname}
+end
+
+post '/age' do
+	firstname = params[:firstname]
+	lastname = params[:lastname]
+	age = params[:age]
+	"your name is #{firstname} #{lastname} and you are #{age} years old"
 end
