a1f944ac6b646bf6b2d150837081c4a5960ddc49
jkeiffer1 <saros82@hotmail.com>
2017-07-25 19:09:09 -0400

    added favorite numbers, food, and drink. and a results page

--- app2.rb
+++ app2.rb
@@ -38,5 +38,98 @@ post '/hair' do
 	lastname = params[:lastname]
 	age = params[:age]
 	hair = params[:hair]
-	"your name is #{firstname} #{lastname}, you are #{age} years old, and you have #{hair} hair."
+#	"your name is #{firstname} #{lastname}, you are #{age} years old, and you have #{hair} hair."
+redirect '/eyes?firstname=' + firstname + "&lastname=" + lastname + "&age=" + age + "&hair=" + hair
+end
+
+get '/eyes' do
+	firstname = params[:firstname]
+	lastname = params[:lastname]
+	age = params[:age]
+	hair = params[:hair]
+	erb :eyes, locals:{firstname: firstname, lastname: lastname, age: age, hair: hair}
+end
+
+post '/eyes' do
+	firstname = params[:firstname]
+	lastname = params[:lastname]
+	age = params[:age]
+	hair = params[:hair]
+	eyes = params[:eyes]
+#	"your name is #{firstname} #{lastname}, you are #{age} years old, and you have #{hair} hair and #{eyes} eyes."
+redirect '/food_drink?firstname=' + firstname + "&lastname=" + lastname + "&age=" + age + "&hair=" + hair + "&eyes=" + eyes
+end
+
+get '/food_drink' do
+	firstname = params[:firstname]
+	lastname = params[:lastname]
+	age = params[:age]
+	hair = params[:hair]
+	eyes = params[:eyes]
+	erb :food_drink, locals:{firstname: firstname, lastname: lastname, age: age, hair: hair, eyes: eyes}
+end
+
+post '/food_drink' do
+	firstname = params[:firstname]
+	lastname = params[:lastname]
+	age = params[:age]
+	hair = params[:hair]
+	eyes = params[:eyes]
+	food = params[:food]
+	drink = params[:drink]
+#	"your name is #{firstname} #{lastname}, you are #{age} years old, and you have #{hair} hair and #{eyes} eyes. You like #{food} and #{drink}"
+redirect '/fnums?firstname=' + firstname + "&lastname=" + lastname + "&age=" + age + "&hair=" + hair + "&eyes=" + eyes + "&food=" + food + "&drink=" + drink
+end
+
+get '/fnums' do
+	firstname = params[:firstname]
+	lastname = params[:lastname]
+	age = params[:age]
+	hair = params[:hair]
+	eyes = params[:eyes]
+	food = params[:food]
+	drink = params[:drink]
+	erb :fnums, locals:{firstname: firstname, lastname: lastname, age: age, hair: hair, eyes: eyes, food: food, drink: drink}
+end
+
+post '/fnums' do
+	firstname = params[:firstname]
+	lastname = params[:lastname]
+	age = params[:age]
+	hair = params[:hair]
+	eyes = params[:eyes]
+	food = params[:food]
+	drink = params[:drink]
+	num1 = params[:num1]
+	num2 = params[:num2]
+	num3 = params[:num3]
+#	"your name is #{firstname} #{lastname}, you are #{age} years old, and you have #{hair} hair and #{eyes} eyes. You like #{food} and #{drink}, and your 3 favorite numbers are #{num1}, #{num2}, and #{num3}."
+redirect '/results?firstname=' + firstname + "&lastname=" + lastname + "&age=" + age + "&hair=" + hair + "&eyes=" + eyes + "&food=" + food + "&drink=" + drink +"&num1=" + num1 + "&num2=" + num2 + "&num3=" + num3
+end
+
+get '/results' do
+	firstname = params[:firstname]
+	lastname = params[:lastname]
+	age = params[:age]
+	hair = params[:hair]
+	eyes = params[:eyes]
+	food = params[:food]
+	drink = params[:drink]
+	num1 = params[:num1]
+	num2 = params[:num2]
+	num3 = params[:num3]
+	erb :results, locals:{firstname: firstname, lastname: lastname, age: age, hair: hair, eyes: eyes, food: food, drink: drink, num1: num1, num2: num2, num3: num3}
+end
+
+post '/results' do
+	firstname = params[:firstname]
+	lastname = params[:lastname]
+	age = params[:age]
+	hair = params[:hair]
+	eyes = params[:eyes]
+	food = params[:food]
+	drink = params[:drink]
+	num1 = params[:num1]
+	num2 = params[:num2]
+	num3 = params[:num3]
 end
